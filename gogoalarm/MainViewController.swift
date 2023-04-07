3eae41430dbf296d4828e3fbb571d77e89ba0ab2
gogochang <okpo2581@gmail.com>
2023-04-07 19:49:57 +0900

    feat,design(main) : 알람추가버튼 클릭 시 관련버튼 이외 비활성화 표시하는 UI 및 기능 추가
    addBtn 클릭 시 배경인 backBtn을 검정 반투명으로 비활성화 되도록 표시, Alarm 추가 버튼 표시
    배경 및 addBtn 버튼 다시 클릭하면 원상태로 전환

--- gogoalarm/MainViewController.swift
+++ gogoalarm/MainViewController.swift
@@ -14,6 +14,8 @@ class MainViewController: UIViewController, UITableViewDelegate {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var settingBtn: UIButton!
     @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var addAlarmBtn: UIButton!
+    @IBOutlet weak var backBtn: UIButton!
     @IBOutlet weak var alarmTableView: UITableView!
     
     //TableView Items
@@ -23,6 +25,9 @@ class MainViewController: UIViewController, UITableViewDelegate {
     //Alarm Data
     var alarms: [AlarmData] = [AlarmData(count: 10, time: "00:00")]
     var data = Array(1...3)
+    
+    //Alarm Add Button
+    var isAddBtnClicked: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,16 +40,45 @@ class MainViewController: UIViewController, UITableViewDelegate {
 
 //MARK: Action
 extension MainViewController {
-    //AddButton Action
+    //AddButton
     @IBAction func addBtnClicked(_ sender: UIButton) {
         print("MainViewController - addBtnClicked() called")
-        UIView.animate(withDuration: 0.3) {
-            if self.addBtn.transform == CGAffineTransform.identity {
-                self.addBtn.transform = CGAffineTransform(rotationAngle: CGFloat.pi/4)
-            } else {
-                self.addBtn.transform = CGAffineTransform.identity
-            }
+        if isAddBtnClicked {
+            showUI()
+        } else {
+            hideUI()
         }
+    }
+    
+    @IBAction func addAlarmBtnClicked(_ sender: UIButton) {
+        print("MainViewController - addAlarmBtnClicked() called")
+    }
+    
+    @IBAction func backBtnClicked(_ sender: UIButton) {
+        print("MainViewController - backBtnClicked() called")
+        self.showUI()
+    }
+    
+    // AddButton 클릭 시 비활성화 표시하는 UI 가리기
+    private func showUI() {
+        print("MainViewController - hide() called")
+        UIView.animate(withDuration: 0.3) {
+            self.addBtn.transform = CGAffineTransform.identity
+            self.addAlarmBtn.alpha = 0.0
+            self.backBtn.alpha = 0
+        }
+        self.isAddBtnClicked = false
+    }
+    
+    // AddButton 클릭 시 비활성화 표시하는 UI 보여주기
+    private func hideUI() {
+        print("MainViewController - show() called")
+        UIView.animate(withDuration: 0.3) {
+            self.addBtn.transform = CGAffineTransform(rotationAngle: CGFloat.pi/4)
+            self.addAlarmBtn.alpha = 1
+            self.backBtn.alpha = 0.5
+        }
+        self.isAddBtnClicked = true
     }
 }
 
@@ -70,7 +104,13 @@ extension MainViewController {
         addBtn.setImage(UIImage(systemName: "plus"), for: .normal)
         addBtn.layer.cornerRadius = addBtn.frame.size.width / 2.0
         addBtn.clipsToBounds = true
+
+        addAlarmBtn.setImage(UIImage(systemName: "alarm.fill"), for: .normal)
+        addAlarmBtn.layer.cornerRadius = addBtn.frame.size.width / 2.0
+        addAlarmBtn.clipsToBounds = true
+        
     }
+    
 }
 
 //MARK: TableView
--- gogoalarm/TableViewCell/AlarmInfoViewCell.swift
+++ gogoalarm/TableViewCell/AlarmInfoViewCell.swift
@@ -12,17 +12,11 @@ class AlarmInfoViewCell: UITableViewCell {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var alarmSwitch: UISwitch!
     
-    
-    private var didSelected: Bool = false
-    
-    
     @IBAction func switchToggled(_ sender: UISwitch) {
         if sender.isOn {
-            print("2")
             contentView.backgroundColor = UIColor.white.withAlphaComponent(1)
         } else {
             contentView.backgroundColor = UIColor.white.withAlphaComponent(0.5)
-            print("#")
         }
     }
     
@@ -33,19 +27,25 @@ class AlarmInfoViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        contentView.layer.cornerRadius = 8
+        
+        // Cell 배경 불투명
         contentView.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        
+        // Cell 모서리
+        contentView.layer.cornerRadius = 8
+        
+        // Cell 그림자
         contentView.layer.shadowColor = UIColor.black.cgColor //색상
         contentView.layer.shadowOpacity = 0.3 //alpha값
         contentView.layer.shadowRadius = 5 //반경
         contentView.layer.shadowOffset = CGSize(width: 1, height: 1) //위치조정
         contentView.layer.masksToBounds = false
-        
+
+        // Cell 크기 조절 ( 안으로 5만큼씩 )
         contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top:5.0, left: 5.0, bottom: 5.0, right: 5.0))
     }
     
